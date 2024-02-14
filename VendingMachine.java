e6e44344175d80c48683d119b8af1591de3f49da
Nikolai Nemkov <144113116+Nikolai-Nemkov@users.noreply.github.com>
2024-02-14 15:32:11 +0300

    Add files via upload

---
+++ VendingMachine.java
@@ -0,0 +1,33 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface VendingMachine {     // торговый автомат
+
+    void initProduct(List <Product> List);
+
+    void initProducts(List<Product> List);
+
+    Product getProduct(String name);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
